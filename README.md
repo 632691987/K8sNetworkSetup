4e9293569130bc52c688f1c6da5088dc205b09b6
Vincent <vincentzhang@outlook.es>
2021-07-26 22:18:46 +0200

    Projct init

---
+++ README.md
@@ -0,0 +1,2 @@
+1. run ```mvn clean install```
+2. run ```docker-compose up --build```
---
+++ src/main/java/com/example/k8snetworksetup/K8sNetworkSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8snetworksetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sNetworkSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sNetworkSetupApplication.class, args);
+    }
+
+}
